42483067ff981b5524c04c83587abc66ffdc28e8
Suelen Critina Vaz <sucris12@gmail.com>
2024-08-05 17:42:08 -0300

    feature/03 Setting tableView

--- gist-peek/gist-peek/ListViewController.swift
+++ gist-peek/gist-peek/ListViewController.swift
@@ -8,10 +8,63 @@
 import UIKit
 
 class ListViewController: UIViewController {
+    // MARK: - Property(ies).
+    private lazy var mainView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        view.addSubview(tableView)
+        return view
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.scrollsToTop = true
+        tableView.backgroundColor = .white
+        return tableView
+    }()
 
+    // MARK: - Override(s).
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Lista"
         view.backgroundColor = .cyan
+        view.addSubview(mainView)
+        configTableView()
+        configConstraints()
+    }
+    
+    // MARK: - Method(s).
+    private func configTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            mainView.topAnchor.constraint(equalTo: view.topAnchor),
+            mainView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            tableView.topAnchor.constraint(equalTo: mainView.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: mainView.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: mainView.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: mainView.bottomAnchor)
+        ])
+    }
+}
+// MARK: - Extension(s).
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Linha \(indexPath.row)"
+        return cell
     }
 }
